c7ab8675121826b87dbae0636c0102afccda8e0f
L. Xu <utvaz@student.kit.edu>
2022-05-08 12:48:39 +0200

    The texture atlas now loads from ressource folder getNormCoord was modified setSubimage should not only write on the diagonal anymore

--- src/main/java/edu/kit/scc/git/ggd/voxelite/texture/TextureAtlas.java
+++ src/main/java/edu/kit/scc/git/ggd/voxelite/texture/TextureAtlas.java
@@ -1,5 +1,6 @@
 package edu.kit.scc.git.ggd.voxelite.texture;
 
+import edu.kit.scc.git.ggd.voxelite.objects.Block;
 import net.durchholz.beacon.math.Vec2f;
 import net.durchholz.beacon.render.opengl.textures.GLTexture;
 import net.durchholz.beacon.render.opengl.textures.Texture2D;
@@ -7,6 +8,7 @@ import net.durchholz.beacon.util.Image;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Arrays;
 import java.util.List;
 
@@ -16,15 +18,16 @@ public class TextureAtlas {
     int id;
     Texture2D texture;
 
-    static int subImageWidth = 16;
-    static int subImageHeight = 16;
+    public static int subImageWidth = 16;
+    public static int subImageHeight = 16;
     static int sheetWidth = subImageWidth * subImageWidth;
     static int sheetHeight = subImageHeight * subImageHeight;
 
     static int rowNumber = sheetWidth / subImageWidth;
     static int columnNumber = sheetHeight / subImageHeight;
 
-    final File folder = new File("C:\\Users\\Gabu\\IdeaProjects\\ggd-voxelite\\src\\main\\resources\\texture.atlas\\wool");
+    URL url = ClassLoader.getSystemResource("resources/texture/atlas/wool");
+    final File folder = new File(url.getFile());
     final List<File> fileList = Arrays.asList(folder.listFiles());
 
     public TextureAtlas() {
@@ -36,18 +39,23 @@ public class TextureAtlas {
     public void setSubImage() throws IOException {
         for (int i = 0; i < fileList.size(); i++) {
             Image img = new Image(fileList.get(i));
-            texture.subImage(img, i * subImageWidth, i * subImageHeight);
+            texture.subImage(img, (i % rowNumber) * subImageWidth, (i / columnNumber) * subImageHeight);
         }
     }
 
-    //returns the normalized coordinates of the given coordinates in the sheet
-    public Vec2f getNormCoord(Vec2f vec) {
-        float x = vec.x() / sheetWidth;
-        float y = vec.y() / sheetHeight;
-        return new Vec2f(x,y);
+    //returns the normalized coordinates of the given coordinates in the sheet fot the block type
+    public static Vec2f[] getNormCoord(Block.Type blockType) {
+        Vec2f[] coord = new Vec2f[4];
+        //bottom left
+        coord[0] = new Vec2f(((blockType.id() % rowNumber) * subImageWidth) / sheetWidth, ((blockType.id() / rowNumber) * subImageHeight) / sheetHeight);
+        //bottom right
+        coord[1] = new Vec2f((((blockType.id() % rowNumber) + 1) * subImageWidth) / sheetWidth, ((blockType.id() / rowNumber) * subImageHeight) / sheetHeight);
+        //top left
+        coord[2] = new Vec2f(((blockType.id() % rowNumber) * subImageWidth) / sheetWidth, (((blockType.id() / rowNumber) + 1) * subImageHeight) / sheetHeight);
+        //top right
+        coord[3] = new Vec2f((((blockType.id() % rowNumber) + 1) * subImageWidth) / sheetWidth, (((blockType.id() / rowNumber) + 1) * subImageHeight) / sheetHeight);
+        return coord;
     }
-
-
 
 
 }
