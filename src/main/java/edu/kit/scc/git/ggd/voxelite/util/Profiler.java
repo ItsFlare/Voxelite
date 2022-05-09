0943678b6d1b38a2c1c5aeda349c3ebf5d3d6e87
Flare <flarefps@gmail.com>
2022-05-09 16:47:38 +0200

    Optimize frame time profiler (#1)

--- src/main/java/edu/kit/scc/git/ggd/voxelite/util/Profiler.java
+++ src/main/java/edu/kit/scc/git/ggd/voxelite/util/Profiler.java
@@ -3,7 +3,9 @@ package edu.kit.scc.git.ggd.voxelite.util;
 import java.util.stream.LongStream;
 
 public class Profiler {
-    private static final int BUFFER_SIZE = 100;
+    private static final int BUFFER_SIZE_EXP = 7;
+    private static final int BUFFER_SIZE = 1 << BUFFER_SIZE_EXP;
+    private static final int MASK = BUFFER_SIZE - 1;
 
     private final long[] frameTime = new long[BUFFER_SIZE];
 
@@ -11,7 +13,7 @@ public class Profiler {
     private int  tick;
 
     public void tick() {
-        frameTime[tick++ % 100] = System.nanoTime() - frameStart;
+        frameTime[tick++ & MASK] = System.nanoTime() - frameStart;
         frameStart = System.nanoTime();
     }
 
