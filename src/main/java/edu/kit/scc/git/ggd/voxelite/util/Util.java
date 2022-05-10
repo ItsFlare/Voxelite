8671261507448dc6ebd265aeccc1c76dca21a763
Flare <flarefps@gmail.com>
2022-05-10 13:11:34 +0200

    Add listResourceFolder to Util

--- src/main/java/edu/kit/scc/git/ggd/voxelite/util/Util.java
+++ src/main/java/edu/kit/scc/git/ggd/voxelite/util/Util.java
@@ -2,8 +2,17 @@ package edu.kit.scc.git.ggd.voxelite.util;
 
 import edu.kit.scc.git.ggd.voxelite.Main;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collection;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Util {
 
@@ -13,5 +22,20 @@ public class Util {
 
     public static InputStream readResource(String resource) {
         return Main.class.getClassLoader().getResourceAsStream(resource);
+    }
+
+
+    public static Collection<Path> listResourceFolder(String folder) throws URISyntaxException, IOException {
+        return listResourceFolder(folder, 0);
+    }
+
+    public static Collection<Path> listResourceFolder(String folder, int depth) throws URISyntaxException, IOException {
+        URL url = Util.class.getResource(folder);
+        if(url == null) throw new FileNotFoundException(folder + " not found");
+        Path path = Paths.get(url.toURI());
+
+        try(Stream<Path> walk = Files.walk(path, depth)) {
+            return walk.collect(Collectors.toList());
+        }
     }
 }
