2dae167450dd93d8aa4297f5e8f8ee3fbffc6f68
Flare <flarefps@gmail.com>
2022-05-10 13:54:32 +0200

    Fix listResourceFolder for JAR filesystem

--- src/main/java/edu/kit/scc/git/ggd/voxelite/util/Util.java
+++ src/main/java/edu/kit/scc/git/ggd/voxelite/util/Util.java
@@ -2,14 +2,19 @@ package edu.kit.scc.git.ggd.voxelite.util;
 
 import edu.kit.scc.git.ggd.voxelite.Main;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.*;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Util {
-
-    public static String readShaderResource(String name) throws IOException {
-        return readStringResource("shaders/" + name);
-    }
 
     public static String readStringResource(String resource) throws IOException {
         return new String(readResource(resource).readAllBytes());
@@ -19,12 +24,27 @@ public class Util {
         return Main.class.getClassLoader().getResourceAsStream(resource);
     }
 
-    public static int[] incrementalArray(int size) {
-        int[] result = new int[size];
-        for (int i = 0; i < size; i++) {
-            result[i] = i;
+
+    public static Collection<Path> listResourceFolder(String folder) throws URISyntaxException, IOException {
+        return listResourceFolder(folder, 1);
+    }
+
+    public static Collection<Path> listResourceFolder(String folder, int depth) throws URISyntaxException, IOException {
+        final URL url = Util.class.getResource(folder);
+        if(url == null) throw new FileNotFoundException(folder + " not found");
+
+        final URI uri = url.toURI();
+        Path path;
+
+        try {
+            path = Paths.get(uri);
+        } catch (FileSystemNotFoundException e) {
+            var fs = FileSystems.newFileSystem(uri, Collections.emptyMap());
+            path = fs.getPath(folder);
         }
 
-        return result;
+        try(Stream<Path> walk = Files.walk(path, depth)) {
+            return walk.collect(Collectors.toList());
+        }
     }
 }
