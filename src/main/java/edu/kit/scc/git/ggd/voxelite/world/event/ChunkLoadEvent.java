4e803985ae84317f553cd7cb452b518b90121ef3
Flare <flarefps@gmail.com>
2022-05-12 15:20:14 +0200

    Convert ChunkEvents to record

--- src/main/java/edu/kit/scc/git/ggd/voxelite/world/event/ChunkLoadEvent.java
+++ src/main/java/edu/kit/scc/git/ggd/voxelite/world/event/ChunkLoadEvent.java
@@ -4,18 +4,8 @@ import edu.kit.scc.git.ggd.voxelite.world.Chunk;
 import net.durchholz.beacon.event.Event;
 import net.durchholz.beacon.event.EventType;
 
-public class ChunkLoadEvent implements Event {
+public record ChunkLoadEvent(Chunk chunk) implements Event {
     public static final EventType<ChunkLoadEvent> TYPE = new EventType<>(ChunkLoadEvent.class);
-
-    private final Chunk chunk;
-
-    public ChunkLoadEvent(Chunk chunk) {
-        this.chunk = chunk;
-    }
-
-    public Chunk getChunk() {
-        return chunk;
-    }
 
     @Override
     public EventType<ChunkLoadEvent> getType() {
--- src/main/java/edu/kit/scc/git/ggd/voxelite/world/event/ChunkUnloadEvent.java
+++ src/main/java/edu/kit/scc/git/ggd/voxelite/world/event/ChunkUnloadEvent.java
@@ -4,18 +4,8 @@ import edu.kit.scc.git.ggd.voxelite.world.Chunk;
 import net.durchholz.beacon.event.Event;
 import net.durchholz.beacon.event.EventType;
 
-public class ChunkUnloadEvent implements Event {
+public record ChunkUnloadEvent(Chunk chunk) implements Event {
     public static final EventType<ChunkUnloadEvent> TYPE = new EventType<>(ChunkUnloadEvent.class);
-
-    private final Chunk chunk;
-
-    public ChunkUnloadEvent(Chunk chunk) {
-        this.chunk = chunk;
-    }
-
-    public Chunk getChunk() {
-        return chunk;
-    }
 
     @Override
     public EventType<ChunkUnloadEvent> getType() {
