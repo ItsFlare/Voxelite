8f435a174cce5f29989a5e3faa95bf36e37dc4d0
Flare <flarefps@gmail.com>
2022-07-09 00:19:24 +0200

    Extract CSM into include

--- src/main/resources/shaders/chunk_opaque.fs
+++ src/main/resources/shaders/chunk_opaque.fs
@@ -1,4 +1,7 @@
 #version 410
+#extension GL_ARB_shading_language_include : require
+#include "csm.glsl"
+
 layout(location = 0) out vec3 color;
 layout(location = 1) out vec3 normal;
 layout(location = 2) out vec3 mer;
@@ -12,7 +15,6 @@ in vec3 ViewSpacePos;
 flat in mat3 TBN;
 in float aoFactor;
 
-uniform sampler2DArrayShadow shadowMap;
 uniform sampler2DArray atlas;
 uniform vec3 camera;
 uniform mat4 view;
@@ -20,7 +22,6 @@ uniform mat4 view;
 uniform float ambientStrength;
 uniform float diffuseStrength;
 uniform float specularStrength;
-uniform float constantBias;
 uniform int phongExponent;
 uniform bool shadows;
 uniform bool normalMapSet;
@@ -28,60 +29,7 @@ uniform bool fogSet;
 uniform int fogRange;
 uniform vec3 fogColor;
 
-uniform struct Light {
-    vec3 direction;
-    vec3 color;
-} light;
-
-const int MAX_CASCADES = 4;
-uniform struct Cascade {
-    float far;
-    vec3 scale;
-    vec3 translation;
-} cascades[MAX_CASCADES];
-
-
-uniform int kernel;
-uniform bool cascadeDebug;
-float kernelFactor = 1 / pow(kernel * 2 + 1, 2);
-
-vec3 debugColor = vec3(0);
-
-float ShadowCalculation(vec3 fragPosLightSpace) {
-    int c;
-    for(c = -1; c++ < MAX_CASCADES; ) {
-        if(gl_FragCoord.z < cascades[c].far) {
-            break;
-        }
-    }
-
-    debugColor = vec3(0, 0, c / float(MAX_CASCADES));
-
-    //Effectively projection to shadow screen space
-    fragPosLightSpace *= cascades[c].scale;
-    fragPosLightSpace += cascades[c].translation;
-    fragPosLightSpace = 0.5 * fragPosLightSpace + 0.5;
-
-    vec2 textureSize = textureSize(shadowMap, 0).xy;
-    vec2 texelSize = 1.0 / textureSize;
-
-    float blocksPerPixel = 2 / (cascades[c].scale.y * textureSize.y);
-    float depthPerBlock = cascades[c].scale.z * -2;
-    float bias = tan(acos(min(abs(dot(Normal, -light.direction)), 1))) * blocksPerPixel * depthPerBlock + constantBias;
-
-    float shadow = 0.0;
-    for(int x = -kernel; x <= kernel; ++x) {
-        for (int y = -kernel; y <= kernel; ++y) {
-            shadow += texture(shadowMap, vec4(fragPosLightSpace.xy + vec2(x, y) * texelSize, c, fragPosLightSpace.z - bias * max((abs(x) + abs(y)), 1))); //TODO Fix PCF overbiasing
-        }
-    }
-
-    shadow *= kernelFactor;
-
-    return shadow;
-}
-
-vec3 DirectionalLight(vec3 normal, vec3 viewDirection) {
+vec3 DirectionalLight(vec3 normal, vec3 viewDirection, float shadow) {
     vec3 color = light.color.rgb;
     vec3 lightDirection = normalize(-light.direction);
 
@@ -92,7 +40,6 @@ vec3 DirectionalLight(vec3 normal, vec3 viewDirection) {
     vec3 halfwayDirection = normalize(lightDirection + viewDirection);
     float spec = pow(max(dot(normal, halfwayDirection), 0.0), phongExponent);
 
-    float shadow = shadows ? ShadowCalculation(LightSpacePos) : 1;
     vec3 ambient  = color * ambientStrength;
     vec3 diffuse  = color * diffuseStrength * diff * shadow;
     vec3 specular = color * specularStrength * spec * shadow;
@@ -127,7 +74,9 @@ void main() {
     vec4 t = texture(atlas, vec3(Tex, 0));
     if(t.a < 0.0001) discard;
 
-    vec3 l = DirectionalLight(n, normalize(camera - Pos)) + BlockLight;
+    vec3 debugColor;
+    float shadow = shadows ? ShadowCalculation(LightSpacePos, Normal, debugColor) : 1;
+    vec3 l = DirectionalLight(n, normalize(camera - Pos), shadow) + BlockLight;
     color = l * t.rgb * aoFactor;
 
     if(cascadeDebug) color += debugColor;
---
+++ src/main/resources/shaders/include/csm.glsl
@@ -0,0 +1,52 @@
+uniform sampler2DArrayShadow shadowMap;
+uniform float constantBias;
+
+uniform struct Light {
+    vec3 direction;
+    vec3 color;
+} light;
+
+const int MAX_CASCADES = 4;
+uniform struct Cascade {
+    float far;
+    vec3 scale;
+    vec3 translation;
+} cascades[MAX_CASCADES];
+
+uniform int kernel;
+uniform bool cascadeDebug;
+float kernelFactor = 1 / pow(kernel * 2 + 1, 2);
+
+float ShadowCalculation(vec3 fragPosLightSpace, vec3 normal, out vec3 debugColor) {
+    int c;
+    for(c = -1; c++ < MAX_CASCADES; ) {
+        if(gl_FragCoord.z < cascades[c].far) {
+            break;
+        }
+    }
+
+    debugColor = vec3(0, 0, c / float(MAX_CASCADES));
+
+    //Effectively projection to shadow screen space
+    fragPosLightSpace *= cascades[c].scale;
+    fragPosLightSpace += cascades[c].translation;
+    fragPosLightSpace = 0.5 * fragPosLightSpace + 0.5;
+
+    vec2 textureSize = textureSize(shadowMap, 0).xy;
+    vec2 texelSize = 1.0 / textureSize;
+
+    float blocksPerPixel = 2 / (cascades[c].scale.y * textureSize.y);
+    float depthPerBlock = cascades[c].scale.z * -2;
+    float bias = tan(acos(min(abs(dot(normal, -light.direction)), 1))) * blocksPerPixel * depthPerBlock + constantBias;
+
+    float shadow = 0.0;
+    for(int x = -kernel; x <= kernel; ++x) {
+        for (int y = -kernel; y <= kernel; ++y) {
+            shadow += texture(shadowMap, vec4(fragPosLightSpace.xy + vec2(x, y) * texelSize, c, fragPosLightSpace.z - bias * max((abs(x) + abs(y)), 1))); //TODO Fix PCF overbiasing
+        }
+    }
+
+    shadow *= kernelFactor;
+
+    return shadow;
+}
