b76a8702918fb66278a5f225763429d24762ce54
Flare <flarefps@gmail.com>
2022-07-08 18:58:26 +0200

    Fix CompressedBlockStorage index arithmetic

--- src/main/java/edu/kit/scc/git/ggd/voxelite/world/CompressedBlockStorage.java
+++ src/main/java/edu/kit/scc/git/ggd/voxelite/world/CompressedBlockStorage.java
@@ -3,12 +3,13 @@ package edu.kit.scc.git.ggd.voxelite.world;
 import edu.kit.scc.git.ggd.voxelite.util.Util;
 
 public class CompressedBlockStorage implements ChunkStorage<Block> {
-    private static final int  BITS_PER_BLOCK_EXP  = Util.log2(Util.log2(Block.values().length));
-    private static final int  BITS_PER_BLOCK      = 1 << BITS_PER_BLOCK_EXP;
-    private static final int  BLOCKS_PER_ELEMENT  = (64 >>> BITS_PER_BLOCK_EXP);
-    private static final long MASK                = (1L << BITS_PER_BLOCK) - 1;
+    private static final int  BITS_PER_BLOCK_EXP     = Util.log2(Util.log2(Block.values().length));
+    private static final int  BITS_PER_BLOCK         = 1 << BITS_PER_BLOCK_EXP;
+    private static final int  BLOCKS_PER_ELEMENT     = (64 >>> BITS_PER_BLOCK_EXP);
+    private static final int  BLOCKS_PER_ELEMENT_EXP = Util.log2(BLOCKS_PER_ELEMENT);
+    private static final long MASK                   = (1L << BITS_PER_BLOCK) - 1;
 
-    private final long[] blocks = new long[Chunk.VOLUME >>> BITS_PER_BLOCK];
+    private final long[] blocks = new long[Chunk.VOLUME >>> BLOCKS_PER_ELEMENT_EXP];
 
     @Override
     public Block get(int linear) {
@@ -26,7 +27,7 @@ public class CompressedBlockStorage implements ChunkStorage<Block> {
     }
 
     private static int index(int linear) {
-        return linear >>> BITS_PER_BLOCK;
+        return linear >>> BLOCKS_PER_ELEMENT_EXP;
     }
 
     private static int shift(int linear) {
